9d4dd0e778d525d7fbb973536768fe51b24b677d
Amirreza Farzan <87432211+jexroid@users.noreply.github.com>
2023-12-14 18:27:15 +0330

    init

---
+++ app/news/news.tsx
@@ -0,0 +1,9 @@
+function News() {
+    return (
+        <main>
+            <h1>news</h1>
+        </main>
+    );
+}
+
+export default News;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,23 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#2563eb",
+          "secondary": "#4f46e5",
+          "accent": "#6ee7b7",
+          "neutral": "#3b82f6",
+          "base-100": "#f3f4f6",
+          "info": "#00d7f8",
+          "success": "#00bd3f",
+          "warning": "#e5b500",
+          "error": "#dc2626",
+        },
+      },
+    ],
+  },
+  plugins: [require('daisyui')],
 }
 export default config
